a739c533ac934f304dee8d3153051dae17538a5a
Brendan Duke <bduke@modular.com>
2024-12-27 10:13:13 -0500

    [SDK] Accept beta argument in LPLayerNorm
    Accept a beta argument in LPLayerNorm.
    This is needed in a follow up for Llama vision correctness where
    previously the beta parameter was initialized to zero instead of the
    trained bias weight.
    As a drive by change field types from `TensorValueLike` to `Weight`.
    
    This was tested by applying the layer hook method from #50988 to the MAX
    and PyTorch Llama vision models and comparing layer outputs.
    
    As a drive by, enable the layer norm unit test, update it to take a
    bias, and move it off of `modular_graph_test`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 7249cb4b9cb766e3f376dea4000d1c70afac6afb

--- pipelines/python/nn/norm/lp_norm.py
+++ pipelines/python/nn/norm/lp_norm.py
@@ -13,9 +13,11 @@
 
 """Layer Normalization layer."""
 
+from __future__ import annotations
+
 from dataclasses import dataclass
 
-from max.graph import TensorValue, TensorValueLike, ops
+from max.graph import TensorValue, Weight, ops
 
 from ..layer import Layer
 
@@ -24,17 +26,23 @@ from ..layer import Layer
 class LPLayerNorm(Layer):
     """Layer normalization block."""
 
-    weight: TensorValueLike
+    weight: Weight
+    bias: Weight | None = None
     eps: float = 1e-6
 
     def __call__(self, input: TensorValue):
         # TODO: AIPIPE-95 Replace with a broadcasting rmo.layer_norm
+        bias = (
+            ops.cast(self.bias, input.dtype)
+            if self.bias
+            # If bias wasn't passed then use bias-less layer norm (beta = 0).
+            else ops.broadcast_to(
+                ops.constant(0.0, input.dtype), shape=(input.shape[-1],)
+            )
+        )
         return ops.layer_norm(
             input,
-            ops.cast(self.weight, input.dtype),
-            ops.broadcast_to(
-                ops.constant(0.0, input.dtype),
-                shape=(input.shape[-1],),
-            ),
-            self.eps,
+            gamma=ops.cast(self.weight, input.dtype),
+            beta=bias,
+            epsilon=self.eps,
         )
